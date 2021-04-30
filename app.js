d8d8f053d771b8ecbbe560365d09101b509313c3
Kevin Mei <kevjinx@gmail.com>
2021-04-29 17:15:53 -0700

    finish mvp

--- app.js
+++ app.js
@@ -7,50 +7,21 @@ const lion = 'url(/images/lion.png'
 //player choose their avata''
 
 
+const winningCondition = () => {
+  winConArr.forEach(group => {
+    if (  group[0].value !== '' &&
+          group[0].value === group[1].value &&
+          group[2].value === group[1].value) {
+      setTimeout(() => {
+        alert(`${group[0].value} wins!!!`)
+      }, 100);
+      winStop = true;
+    }
+  })
+}
+
 
 window.addEventListener('DOMContentLoaded', () => {
-  let turnCount = 2; //starts with 2, even users goes first, even = X, odd = O
-  const countTurn = () => {
-    if (turnCount%2 === 0) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  const allTttBtns = document.querySelectorAll('.ttt-btn');
-
-  for (let i = 0; i < allTttBtns.length; i++) {
-    const btn = allTttBtns[i];
-    btn.addEventListener('click', (event) => {
-      if (event.target.value === '') {
-        turnCount++;
-        if (countTurn()) {
-          event.target.style.backgroundImage = oink;
-          event.target.value = 'player1'
-        } else {
-          event.target.style.backgroundImage = lion;
-          event.target.value = 'player2'
-        }
-      }
-      winningCondition();
-      if (winStop) {
-        return
-      }
-    })
-
-  }
-
-  const newGameBtn = document.querySelector('#new-game')
-  newGameBtn.addEventListener('click', event => {
-    //resetting values for all btns
-    allTttBtns.forEach(btn => {
-      btn.value = ''
-      btn.style.backgroundImage = 'none'
-    })
-  })
-
-
   const col0WinCon = document.querySelectorAll('.col0')
   const col1WinCon = document.querySelectorAll('.col1')
   const col2WinCon = document.querySelectorAll('.col2')
@@ -62,20 +33,50 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const winConArr = [col0WinCon, col1WinCon, col2WinCon, row0WinCon, row1WinCon, row2WinCon, diag0WinCon, diag1WinCon]
 
-  let winStop = false;
+  let turnCount = 2; //starts with 2, even users goes first, even = X, odd = O
 
-  const winningCondition = () => {
-    winConArr.forEach(group => {
-      if (  group[0].value !== '' &&
-            group[0].value === group[1].value &&
-            group[2].value === group[1].value) {
-        setTimeout(() => {
-          alert(`${group[0].value} wins!!!`)
-        }, 100);
-        winStop = true;
+  const countTurn = () => {
+    if (turnCount%2 === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  const allTttBtns = document.querySelectorAll('.ttt-btn');
+
+  for (let i = 0; i < allTttBtns.length; i++) {
+    allTttBtns[i].addEventListener('click', (event) => {
+      if (event.target.value === '') {
+        turnCount++;
+        if (countTurn()) {
+          event.target.style.backgroundImage = oink;
+          event.target.value = 'player1'
+        } else {
+          event.target.style.backgroundImage = lion;
+          event.target.value = 'player2'
+        }
       }
+      winningCondition(); //need a way to stop
+      //????how to remove eventlisteners from all the btns???
     })
   }
+
+  const newGameBtn = document.querySelector('#new-game');
+  newGameBtn.addEventListener('click', event => {
+    //resetting values for all btns
+    allTttBtns.forEach(btn => {
+      btn.value = ''
+      btn.style.backgroundImage = 'none'
+    })
+  })
+
+  const giveUpBtn = document.getElementById('give-up');
+  giveUpBtn.addEventListener('click', event => {
+    window.location.reload();
+  })
+
+
 
 
 
